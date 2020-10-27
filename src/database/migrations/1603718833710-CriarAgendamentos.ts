6e3280752168898d1cdd7bc62e282ff750a4e2d7
Léo Araújo <leocraraujo@gmail.com>
2020-10-27 00:49:44 -0300

    modificação do model de agendamento usando anotações pro typeorm

---
+++ src/database/migrations/1603718833710-CriarAgendamentos.ts
@@ -0,0 +1,35 @@
+import { id } from 'date-fns/locale';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CriarAgendamentos1603718833710
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'agendamentos',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provedor',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('agendamentos');
+  }
+}
--- src/models/agendamentos.ts
+++ src/models/agendamentos.ts
@@ -1,17 +1,21 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn, Timestamp } from 'typeorm';
 
+@Entity('agendamentos')
 class Agendamento {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provedor: string;
 
+  @Column('timestamp with time zone')
   data: Date;
 
-  constructor({ provedor, data }: Omit<Agendamento, 'id'>) {
-    this.id = uuid();
-    this.provedor = provedor;
-    this.data = data;
-  }
+  // constructor({ provedor, data }: Omit<Agendamento, 'id'>) {
+  //   this.id = uuid();
+  //   this.provedor = provedor;
+  //   this.data = data;
+  // }
 }
 
 export default Agendamento;
